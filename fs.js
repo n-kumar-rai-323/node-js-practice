058de6fda918fddc1dff928aba5c82e6f6983a12
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-08-26 08:18:12 -0700

    NODE

--- fs.js
+++ fs.js
@@ -1,5 +1,8 @@
-const fs = require("fs")
-
+const fs = require("fs");
 
 // write a js founction to read the content of the file using fs module
+let text = fs.readFileSync("./hello.txt", "utf-8");
+text = text.replace("Rai", "Computer");
 
+console.log("creating a new file...");
+fs.writeFileSync("vga.txt", text);
