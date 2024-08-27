a5f8fdd2526caace94ab7981eb97284efc7941fe
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-08-27 01:47:37 -0700

    Node

---
+++ proper-case/index.js
@@ -0,0 +1,8 @@
+const properCase = require("proper-upper-case");
+
+console.log(properCase("Nishan RAI "));
+
+// generate QR code for the url on the terminal usig package qrcode.
+// write a nodejs application to convert the currency from one currency to another using package: currencry-converter-it
+// write a nodejs application that can generate pdf usig text images. use package : pdfkit
+// weather API using package axios and open source weather url LINK
