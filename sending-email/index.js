db81fc5ef6e422e022071047324c96cab4dc4a77
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-08-28 23:54:30 -0700

    Email

---
+++ sending-email/index.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: process.env.SMTP_SERVICE,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSWORD,
+  },
+});
+
+// verify connection configuration
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take our messages");
+  }
+});
