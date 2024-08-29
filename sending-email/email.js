d181d36067654b494410b7c7a496dfd3371cbb0f
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-08-29 08:37:27 -0700

    Email

---
+++ sending-email/email.js
@@ -0,0 +1,7 @@
+const { mailer } = require("./index");
+
+mailer({
+  to: "rainisha484@gmail.com",
+  subject: "Hack your FB account ",
+  message: "<h1>Madharchut nisa</h1>",
+});
--- sending-email/index.js
+++ sending-email/index.js
@@ -17,3 +17,15 @@ transporter.verify(function (error, success) {
     console.log("Server is ready to take our messages");
   }
 });
+
+const mailer = async ({ to, subject, message }) => {
+  const result = await transporter.sendMail({
+    from: `"unknow user" <${process.env.SMTP_USER}>`,
+    to,
+    subject,
+    html: message,
+  });
+  return result;
+};
+
+module.exports = { mailer };
